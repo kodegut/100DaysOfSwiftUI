b4e597a0e9a78141299f833ebf70fab428e68a2c
Tim <74768819+kodegut@users.noreply.github.com>
2021-08-01 10:48:40 +0200

    100dos96

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Tim Musil on 01.08.21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
